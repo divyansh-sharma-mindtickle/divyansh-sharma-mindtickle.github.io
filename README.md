8741b7dae1b385df4d92b42f626f137036fa53fa
divyansh-sharma-mindtickle <divyansh.sharma@mindtickle.com>
2024-08-20 17:25:16 +0530

    Automated sync

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Documentation Index
 
-This repository 
-  contains the following Markdown files:
+This repository contains the following Markdown files:
 
 ## List of Files
 
-- [2024-08-20.html](/https://divyansh-sharma-mindtickle.github.io/2024-08-20.html)
-- [Content Code review ( History ).html](/https://divyansh-sharma-mindtickle.github.io/Content%20Code%20review%20(%20History%20).html)
-- [Content Repos - App Mapping.html](/https://divyansh-sharma-mindtickle.github.io/Content%20Repos%20-%20App%20Mapping.html)
-- [PSPDFKIT.html](/https://divyansh-sharma-mindtickle.github.io/PSPDFKIT.html)
-- [cherry-pick.html](/https://divyansh-sharma-mindtickle.github.io/cherry-pick.html)
-- [images available.html](/https://divyansh-sharma-mindtickle.github.io/images%20available.html)
+- [2024-08-20.md](https://divyansh-sharma-mindtickle.github.io/2024-08-20.md)
+- [Content Code review ( History ).md](https://divyansh-sharma-mindtickle.github.io/Content Code review ( History ).md)
+- [Content Repos - App Mapping.md](https://divyansh-sharma-mindtickle.github.io/Content Repos - App Mapping.md)
+- [PSPDFKIT.md](https://divyansh-sharma-mindtickle.github.io/PSPDFKIT.md)
+- [cherry-pick.md](https://divyansh-sharma-mindtickle.github.io/cherry-pick.md)
+- [images available.md](https://divyansh-sharma-mindtickle.github.io/images available.md)
 
 ## How to Use This Repository
 
-Each file contains notes. Click on the links above to navigate to the respective files.
+Each file contains documentation or notes related to specific parts of the project. Click on the links above to navigate to the respective files.
+
+## Contribution Guidelines
+
+Feel free to contribute to the documentation by adding or updating the Markdown files. Make sure to follow the repository's contribution guidelines.
 
 ---
 
--- generate-readme.js
+++ generate-readme.js
@@ -1,46 +1,43 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const directoryPath = path.join(__dirname, ".");
-const outputFilePath = path.join(__dirname, "README.md");
-const baseUrl = "https://divyansh-sharma-mindtickle.github.io/"; // Base URL for GitHub Pages
+const directoryPath = path.join(__dirname, '.');
+const outputFilePath = path.join(__dirname, 'README.md');
+const baseUrl = 'https://divyansh-sharma-mindtickle.github.io/'; // Base URL for GitHub Pages
 
 function getAllMarkdownFiles(dirPath, arrayOfFiles) {
-  const files = fs.readdirSync(dirPath);
+    const files = fs.readdirSync(dirPath);
 
-  arrayOfFiles = arrayOfFiles || [];
+    arrayOfFiles = arrayOfFiles || [];
 
-  files.forEach((file) => {
-    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllMarkdownFiles(dirPath + "/" + file, arrayOfFiles);
-    } else if (path.extname(file) === ".md" && file !== "README.md") {
-      arrayOfFiles.push(path.join(dirPath, "/", file));
-    }
-  });
+    files.forEach((file) => {
+        if (fs.statSync(dirPath + '/' + file).isDirectory()) {
+            arrayOfFiles = getAllMarkdownFiles(dirPath + '/' + file, arrayOfFiles);
+        } else if (path.extname(file) === '.md' && file !== 'README.md') {
+            arrayOfFiles.push(path.join(dirPath, '/', file));
+        }
+    });
 
-  return arrayOfFiles;
+    return arrayOfFiles;
 }
 
 function generateReadmeContent(files) {
-  const fileLinks = files
-    .map((file) => {
-      let relativePath = path.relative(directoryPath, file).replace(/\\/g, "/");
-      relativePath = relativePath.replace(".md", ".html");
-      const url = new URL(relativePath, baseUrl).toString();
-      return `- [${relativePath}](/${url})`;
-    })
-    .join("\n");
+    const fileLinks = files.map(file => {
+        const relativePath = path.relative(directoryPath, file).replace(/\\/g, '/');
+        let fileUrl = `${baseUrl}${relativePath}`;
+        fileUrl = new URL(fileUrl).toString();
+        return `- [${relativePath}](${fileUrl})`;
+    }).join('\n');
 
-  return `# Documentation Index\n\nThis repository 
-  contains the following Markdown files:\n\n## List of Files\n\n${fileLinks}\n\n## How to Use This Repository\n\nEach file contains notes. Click on the links above to navigate to the respective files.\n\n---\n\n*Generated on: ${new Date().toLocaleDateString()}*`;
+    return `# Documentation Index\n\nThis repository contains the following Markdown files:\n\n## List of Files\n\n${fileLinks}\n\n## How to Use This Repository\n\nEach file contains documentation or notes related to specific parts of the project. Click on the links above to navigate to the respective files.\n\n## Contribution Guidelines\n\nFeel free to contribute to the documentation by adding or updating the Markdown files. Make sure to follow the repository's contribution guidelines.\n\n---\n\n*Generated on: ${new Date().toLocaleDateString()}*`;
 }
 
 function generateReadme() {
-  const markdownFiles = getAllMarkdownFiles(directoryPath);
-  const readmeContent = generateReadmeContent(markdownFiles);
+    const markdownFiles = getAllMarkdownFiles(directoryPath);
+    const readmeContent = generateReadmeContent(markdownFiles);
 
-  fs.writeFileSync(outputFilePath, readmeContent, "utf8");
-  console.log("README.md file has been generated.");
+    fs.writeFileSync(outputFilePath, readmeContent, 'utf8');
+    console.log('README.md file has been generated.');
 }
 
 generateReadme();
